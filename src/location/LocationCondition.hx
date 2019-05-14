e909b0a3818c223c62717896d2673874e1fa7db1
P.J.Shand <pete@peteshand.net>
2019-05-15 09:59:21 +1000

    Fix cloning

--- src/location/LocationCondition.hx
+++ src/location/LocationCondition.hx
@@ -2,55 +2,50 @@ package location;
 
 import condition.Condition;
 
-class LocationCondition extends Condition
-{
+class LocationCondition extends Condition {
 	var locations = new Condition();
 	var maskLocations = new Condition();
-	
-	public function new() 
-	{
+
+	public function new() {
 		super();
 		this.add(locations.active, "==", true);
 		this.add(maskLocations.active, "==", true);
 	}
 
-	public function addLocation(location:String, wildcard:Bool=false):Void 
-	{
+	public function addLocation(location:String, wildcard:Bool = false):Void {
 		locations.or();
 		locations.add(Location.instance, "==", location, wildcard);
 	}
-	
-	public function addLocationMask(location:String, wildcard:Bool=false):Void 
-	{
+
+	public function addLocationMask(location:String, wildcard:Bool = false):Void {
 		maskLocations.and();
 		maskLocations.add(Location.instance, "!=", location, wildcard);
 	}
-	
-	public function removeLocation(location:String):Void
-	{
-        locations.remove(Location.instance, "==", location);
-	}
-	
-	public function removeLocationMask(location:String):Void
-	{
-        maskLocations.remove(Location.instance, "!=", location);
+
+	public function removeLocation(location:String):Void {
+		locations.remove(Location.instance, "==", location);
 	}
 
-	override public function clone()
-	{
+	public function removeLocationMask(location:String):Void {
+		maskLocations.remove(Location.instance, "!=", location);
+	}
+
+	override public function clone() {
 		var _clone = new LocationCondition();
 		copyCases(this, _clone);
+		copyCases(this.locations, _clone.locations);
+		copyCases(this.maskLocations, _clone.maskLocations);
 		_clone.check();
 		return _clone;
 	}
 
-	override function toString():String
-	{
+	override function toString():String {
 		var s:String = locations.toString() + "\n";
 		s += numCases + " (";
 		for (i in 0...cases.length) {
 			s += cases[i];
-			if (i < cases.length-1) s += " " + cases[i].bitOperator + " ";
+			if (i < cases.length - 1)
+				s += " " + cases[i].bitOperator + " ";
 		}
 		s += ") :: value = " + value;
 		return s;
